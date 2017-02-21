09838499bec03bee593c0f084fce69576233fe2e
Christopher Lewis <chris.lewis@hudl.com>
2017-02-21 20:46:15 +0000

    use gulp-util instead

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,20 +1,20 @@
 var webpack = require('webpack');
 var config = require('./webpack.config.js');
 var gulp = require('gulp');
-var gutil = require('gutil');
-gulp.task('default', function(done) {
-    webpack(config).run(onBuild(done));
+var gutil = require('gulp-util');
+gulp.task('default', function (done) {
+    webpack(config).watch(undefined, onBuild(done));
 });
 
 function onBuild(done) {
-    return function(err, stats) {
+    return function (err, stats) {
         if (err) {
             gutil.log('Error', err);
             if (done) {
                 done();
             }
         } else {
-            Object.keys(stats.compilation.assets).forEach(function(key) {
+            Object.keys(stats.compilation.assets).forEach(function (key) {
                 gutil.log('Webpack: output ', gutil.colors.green(key));
             });
             gutil.log('Webpack: ', gutil.colors.blue('finished ', stats.compilation.name));
