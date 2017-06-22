fa4f0ae394bef279d5155cbe5513645630d40224
Chris Lewis <cdlewis01@gmail.com>
2017-06-22 22:04:54 +0100

    some changes

--- src/js/base.tsx
+++ src/js/base.tsx
@@ -3,10 +3,10 @@ import * as ReactDOM from 'react-dom';
 import * as $ from 'jquery';
 import * as Bootstrap from 'bootstrap';
 
-export class MenuItem{
+export class MenuItem {
     name: string;
     url: string;
-    constructor(name: string, url:string){
+    constructor(name: string, url: string) {
         this.name = name;
         this.url = url;
     }
@@ -16,7 +16,7 @@ export class NavBarProperties {
     menuItems: MenuItem[];
     name: string;
     selected: string;
-    constructor(menuNames: MenuItem[], name: string, selected: string){
+    constructor(menuNames: MenuItem[], name: string, selected: string) {
         this.menuItems = menuNames;
         this.name = name;
         this.selected = selected;
@@ -24,64 +24,46 @@ export class NavBarProperties {
 }
 
 export class NavBar extends React.Component<NavBarProperties, undefined> {
-    render(){
+    render() {
         var menus = new Array<any>();
-        this.props.menuItems.map(i=>{
-            menus.push((<li className={this.props.selected === i.name?"active":""}><a href={i.url}>{i.name}</a></li>));
+        this.props.menuItems.map(i => {
+            menus.push((<li className={this.props.selected === i.name ? "active" : ""}><a href={i.url}>{i.name}</a></li>));
         });
         return (<nav className="navbar navbar-default navbar-static-top">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button className="navbar-toggle collapsed" aria-expanded="false" aria-controls="navbar" type="button" data-toggle="collapse" data-target="#navbar">
-                                <span className="sr-only"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <a className="navbar-brand" href="/Home">{this.props.name}</a>
-                        </div>
-                        <div className="navbar-collapse collapse" id="navbar">
-                            <ul className="nav navbar-nav">
-                                {menus}   
-                            </ul>
-                        </div>
-                    </div>
-                </nav>);
-    }
-}
-
-class Profile extends React.Component<undefined, undefined>{
-    render(){
-         return <div className="thumbnail">
-                    <img src="./images/headshot.jpg" alt="headshot" />
-                    <div className="caption">
-                        <h3>Profile</h3>
-                        <p>Piano BMus RSAMD - 2010</p>
-                        <p>ERASMUS Vienna</p>
-                        <p>Primary School Class Teacher</p>
-                        <p>Primary School Music Teacher</p>
-                    </div>    
+            <div className="container">
+                <div className="navbar-header">
+                    <button className="navbar-toggle collapsed" aria-expanded="false" aria-controls="navbar" type="button" data-toggle="collapse" data-target="#navbar">
+                        <span className="sr-only"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="/Home"><img className="brand-image" src="./images/favicon.png" /></a>
                 </div>
+                <div className="navbar-collapse collapse" id="navbar">
+                    <ul className="nav navbar-nav">
+                        {menus}
+                    </ul>
+                </div>
+            </div>
+        </nav>);
     }
 }
 
 export class BodyLayout extends React.Component<undefined, undefined>{
-    render(){
+    render() {
         return <div className="container-fluid">
-                   <div className="row">
-                        <div className="col-xs-12 col-s-12 col-md-8 col-lg-8">
-                            {this.props.children}
-                        </div>
-                        <div className="col-md-4 col-lg-4">
-                            <Profile />
-                        </div>
-                    </div>
+            <div className="row">
+                <div className="col-xs-12 col-s-12 col-md-12 col-lg-12">
+                    {this.props.children}
                 </div>
+            </div>
+        </div>
     }
 }
 
 var menuItems = new Array<MenuItem>();
-menuItems.push(new MenuItem("Repertoire", "/Repertoire"), 
-               new MenuItem("Teaching", "/Teaching"),
-               new MenuItem("Contact", "/Contact"));
-export {menuItems as MenuItems};
+menuItems.push(new MenuItem("Repertoire", "/Repertoire"),
+    new MenuItem("Teaching", "/Teaching"),
+    new MenuItem("Contact", "/Contact"));
+export { menuItems as MenuItems };
--- src/js/home/index.tsx
+++ src/js/home/index.tsx
@@ -2,35 +2,137 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as _ from 'jquery';
 import * as Bootstrap from 'bootstrap';
-import {NavBar, MenuItems, BodyLayout} from '../base';
-class Description extends React.Component<undefined, undefined>{
-        render(){
-            return <div className="jumbotron">
-                        <h1>Piano Teaching in Wales</h1>
-                        <p>Overview Description of what I have done and what I do</p> 
-                    </div>
-        }
-}
+import { NavBar, MenuItems, BodyLayout } from '../base';
 
-class TestimonialQuote{
+class TestimonialQuote {
     quote: string;
-    who : string;
+    who: string;
 }
 
 class Testimonial extends React.Component<TestimonialQuote, undefined>{
-    render(){
+    render() {
         return <blockquote className="testimonial">
-                    <p>"{this.props.quote}"</p>
-                    <footer>{this.props.who}</footer>
-                    </blockquote>
+            <p>"{this.props.quote}"</p>
+            <footer>{this.props.who}</footer>
+        </blockquote>
     }
 }
 
+class BlogEntryProps {
+    title: string;
+    imageUrl: string;
+    text: string;
+}
+
+
+
+class Header extends React.Component<undefined, undefined>{
+    render() {
+        return (
+            <div className="header">
+                <div className="header-background" />
+                <div className="profile-image-container">
+                    <img src="./images/headshot.jpg" alt="headshot" />
+                </div>
+            </div>);
+    }
+}
+
+class BlogPost {
+    title: string;
+    date: Date;
+    contents: JSX.Element;
+    image_url: string;
+    constructor(title: string, date: Date, contents: JSX.Element, image: string) {
+        this.title = title;
+        this.date = date;
+        this.contents = contents;
+        this.image_url = image;
+    }
+}
+
+class BlogProperties {
+    blogPosts: Array<BlogPost>;
+}
+
+class Blog extends React.Component<BlogProperties, undefined>{
+    render() {
+        var blogs = new Array<any>();
+        this.props.blogPosts.map(bp => {
+            blogs.push((
+                <div className="blog-post">
+                    <div className="page-header">
+                        <div>
+                            <h2 className="blog-post-title">{bp.title}</h2>
+                            <h5>{bp.date.toDateString()}</h5>
+                        </div>
+                    </div>
+                    <div className="blog-content media">
+                        <div className="media-left">
+                            <a href="#">
+                                <img className="media-object" src={bp.image_url} />
+                            </a>
+                        </div>
+                        <div className="media-body">
+                            {bp.contents}
+                        </div>
+                    </div >
+                </div>
+            ));
+        });
+        return (
+            <div className="blog container-fluid">
+                <div className="row text-center">
+                    <div className="col-md-12">
+                        <div className="blog-header">
+                            <h1 className="blog-title">Lucie's Blog</h1>
+                            <p className="blog-description">What I get up to with the piano</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-1">
+                        {blogs}
+                    </div>
+                    <div className="col-md-2">
+                        <div className="profile-container">
+                            <div className="page-header">
+                                <img src="./images/piano-512.jpg" />
+                            </div>
+                            <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis suscipit</p>
+                            <ol>
+                                <li>Qualification 1</li>
+                                <li>Qualification 2</li>
+                            </ol>
+
+                        </div>
+                    </div>
+                </div>
+            </div >);
+    }
+
+}
+var blog1content =
+    <div>
+        <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis suscipit felis eget congue. Morbi sapien urna, lacinia sed semper eu, auctor eget enim. Sed viverra lacus ipsum, ut volutpat sem pulvinar non. Vestibulum id dictum mi. Nullam a quam gravida, ultricies elit non, porttitor sem. Nam ac enim vehicula, facilisis leo et, fringilla est. Curabitur et faucibus magna. Suspendisse sed interdum risus. Mauris sit amet nulla ex. Integer et libero vel quam feugiat dictum ac pellentesque felis. Proin laoreet felis erat, sed efficitur sem mattis non. Morbi sit amet congue nunc. Donec iaculis lectus id neque interdum, et elementum ex semper. Donec ac felis vel metus luctus ultrices. Aenean volutpat magna erat, sed lobortis augue porttitor lacinia. Duis et convallis neque.</p>
+    </div>;
+
+var blog2content =
+    <div>
+        <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis suscipit felis eget congue. Morbi sapien urna, lacinia sed semper eu, auctor eget enim. Sed viverra lacus ipsum, ut volutpat sem pulvinar non. Vestibulum id dictum mi. Nullam a quam gravida, ultricies elit non, porttitor sem. Nam ac enim vehicula, facilisis leo et, fringilla est. Curabitur et faucibus magna. Suspendisse sed interdum risus. Mauris sit amet nulla ex. Integer et libero vel quam feugiat dictum ac pellentesque felis. Proin laoreet felis erat, sed efficitur sem mattis non. Morbi sit amet congue nunc. Donec iaculis lectus id neque interdum, et elementum ex semper. Donec ac felis vel metus luctus ultrices. Aenean volutpat magna erat, sed lobortis augue porttitor lacinia. Duis et convallis neque.</p>
+    </div>;
+
+var blogPosts = new Array<BlogPost>();
+blogPosts.push(new BlogPost("Cardiff Blues - End of Season Awards", new Date(2017, 6, 5), blog1content, "./images/cardiff_blues.png"));
+blogPosts.push(new BlogPost("Cwmbran Male Voice Choir", new Date(2017, 6, 5), blog2content, "./images/choir.jpg"));
+
 ReactDOM.render((<div>
-                    <NavBar menuItems={MenuItems} name="Lucie Bebbington-Lewis" selected="Home" />
-                    <BodyLayout>
-                        <Description />
-                        <Testimonial quote="She's the best teacher EVER!" who="A. Student" />
-                        <Testimonial quote="She taught me everything I know!" who="A. N. Student" />
-                    </BodyLayout>
-                </div>), document.getElementById('bebbiweb'));
+    <NavBar menuItems={MenuItems} name="Lucie Bebbington-Lewis" selected="Home" />
+    <BodyLayout>
+        <Header />
+        <Blog blogPosts={blogPosts} />
+        {/*<Testimonial quote="She's the best teacher EVER!" who="A. Student" />*/}
+        {/*<Testimonial quote="She taught me everything I know!" who="A. N. Student" />*/}
+        {/*<BlogEntry title="Cwmbran Male Voice Choir" text="Old men singing and chatting." imageUrl="/" />*/}
+    </BodyLayout>
+</div>), document.getElementById('bebbiweb'));
